5c1f97b22ef79c3f718324047702e52453241cd3
Ghanshyam Verma <98162318+Ghanshu03101997@users.noreply.github.com>
2022-05-12 23:19:06 +0530

    Update RAM.v

--- RAM.v
+++ RAM.v
@@ -1,19 +1,17 @@
 `timescale 1ns / 1ps
 
 
-module test_bench(
+module RAM_single_port1(data_out,data_in,write_addr,we,clk);
+input [7:0]data_in;
+input we,clk;
+output reg[7:0]data_out;
+input [5:0]write_addr;
+reg [7:0]ram[63:0];
+always@(posedge clk)begin
+if(we)
+ ram[write_addr]<=data_in;
+ data_out<=ram[write_addr];
+ 
+ end
 
-    );
-    reg [7:0]data_in;
-    reg [5:0]write_addr;
-    reg we,clk;
-    wire [7:0]data_out;
-     RAM_single_port1 GH1(data_out,data_in,write_addr,we,clk);
-     initial begin
-     clk=0; we=0;
-     #10 clk=1; we=1; data_in=3; write_addr=0; 
-     #30 data_in=7; write_addr=1;
-     end
-     always # 5 clk=~clk;
-     endmodule
-    
+endmodule
